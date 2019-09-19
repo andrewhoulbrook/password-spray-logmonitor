dd6099ca348b6afdacdc88e8f0bbe4b750777737
Baba Hao <47100671+ba2baha3o@users.noreply.github.com>
2019-09-19 23:58:25 +0800

    Fix typos

--- passwordFailMonitor.sh
+++ passwordFailMonitor.sh
@@ -19,27 +19,24 @@ HEADER="${HEADER} \n ----------------- \t ----------------- \t -----------------
 
 OUTPUT=""    
 
-# Launch subshell
-{
-  # Loop over each username on the system, fliter user from system accounts based on UID > 999
-  for USR in $(cut -d':' -f 1,3 ${PASSWD} | awk -F ':' '$2>999 { print $1 }')
-  do
-    # Check if the user is also in the logfile with failed login attempts
-    if [[ -z $(grep "${USR}" "${LOGFILE}") ]]
-    then
-      # Create an emtpy line in the output table
-      OUTPUT="${OUTPUT} \n ${USR} \t - \t 0"
-    else
-      # Get corresponding count of failed login attempts and datetime stamp of last attempt
-      FAILS=$(lastb -wFf ${LOGFILE} | grep "${USR}")
-      COUNT=$(echo "${FAILS}" | awk '{ print $1 }' | wc -l | awk '{ print $1 }')
-      DATE=$(echo "${FAILS}" | awk '{ x=$3"-"$4"-"$5"-"$6"-"$7; print x }' | sort -r | head -n 1)
+# Loop over each username on the system, fliter user from system accounts based on UID > 999
+for USR in $(cut -d':' -f 1,3 ${PASSWD} | awk -F ':' '$2>999 { print $1 }')
+do
+  # Check if the user is also in the logfile with failed login attempts
+  if [[ -z $(grep "${USR}" "${LOGFILE}") ]]
+  then
+    # Create an emtpy line in the output table
+    OUTPUT="${OUTPUT} \n ${USR} \t - \t 0"
+  else
+    # Get corresponding count of failed login attempts and datetime stamp of last attempt
+    FAILS=$(lastb -wFf ${LOGFILE} | grep "${USR}")
+    COUNT=$(echo "${FAILS}" | awk '{ print $1 }' | wc -l | awk '{ print $1 }')
+    DATE=$(echo "${FAILS}" | awk '{ x=$3"-"$4"-"$5"-"$6"-"$7; print x }' | sort -r | head -n 1)
 
-      # Create a line in the output table with the count data and datetime stamp
-      OUTPUT="${OUTPUT} \n ${USR} \t ${DATE} \t ${COUNT}"
-    fi
-  done
+    # Create a line in the output table with the count data and datetime stamp
+    OUTPUT="${OUTPUT} \n ${USR} \t ${DATE} \t ${COUNT}"
+  fi
+done < <(echo "${PASSWD}")
 
-  # Format display of output table to the terminal, aligned as column-seperated table.
-  printf "%b" "${HEADER} \n $(printf %b ${OUTPUT} | sort -k 3 -r)" | column -t
-}
+# Format display of output table to the terminal, aligned as column-seperated table.
+printf "%b" "${HEADER} \n $(printf %b ${OUTPUT} | sort -k 3 -r)" | column -t
